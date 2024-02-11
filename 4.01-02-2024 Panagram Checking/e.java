8d74c78a554abd6f0595dd4c06f8060f591faf0c
Bhuvan Shetty <98102293+bh-uv-an@users.noreply.github.com>
2024-02-11 20:42:51 +0530

    Add files via upload

---
+++ 4.01-02-2024 Panagram Checking/e.java
@@ -0,0 +1,16 @@
+package Geeks_For_Geeks;
+
+public class e {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        String sentence = "Bawds jog, flick quartz, vex nymph";
+		        
+		        // Remove spaces from the sentence
+		        String noSpacesSentence = sentence.replaceAll("\\s", "");
+		        
+		        // Print the sentence without spaces
+		        System.out.println("Sentence without spaces: " + noSpacesSentence);
+		    }
+
+}
